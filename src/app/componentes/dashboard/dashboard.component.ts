290e4762964dd237bc96b629f2a2b9fbea868d50
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2021-02-11 12:15:22 -0400

    Subiendo cambios

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  folders: any[] = [
+    {
+      name: 'Estructuras de Datos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Estructuras de Datos',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Introducción a la Informática',
+      updated: new Date('1/28/16'),
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
