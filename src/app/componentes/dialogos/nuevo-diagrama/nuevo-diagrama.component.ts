5070eaa5e025bd7070a5277f182af6a34da9e8da
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2021-02-14 21:08:00 -0400

    Loading screen

--- src/app/componentes/dialogos/nuevo-diagrama/nuevo-diagrama.component.ts
+++ src/app/componentes/dialogos/nuevo-diagrama/nuevo-diagrama.component.ts
@@ -5,12 +5,15 @@ import {FormControl, FormGroup} from '@angular/forms';
 import {Diagrama} from '../../../modelos/diagrama';
 import {AuthService} from '../../../servicios/auth/auth.service';
 import {DashboardService} from '../../../servicios/dashboard/dashboard.service';
+import {NgxSpinnerService} from 'ngx-spinner';
 @Component({
   selector: 'app-nuevo-diagrama',
   templateUrl: './nuevo-diagrama.component.html',
   styleUrls: ['./nuevo-diagrama.component.css']
 })
 export class NuevoDiagramaComponent implements OnInit {
+
+  creating : boolean = false;
 
   createDiagramForm = new FormGroup({
     asignment: new FormControl(''),
@@ -21,9 +24,14 @@ export class NuevoDiagramaComponent implements OnInit {
   constructor(public dialog: MatDialog,
               private router: Router,
               private authService: AuthService,
-              private dashboardService: DashboardService) { }
+              private dashboardService: DashboardService,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+  }
+
+  closeModal(){
+    this.spinner.hide();
   }
 
   async goEditor()  {
@@ -39,10 +47,13 @@ export class NuevoDiagramaComponent implements OnInit {
       owner: this.authService.user.uid
     };
 
+    this.creating=true;
     console.log('Intentando crear: ',diagrama);
-
+    this.spinner.show();
     let sr  = await this.dashboardService.createDiagram(diagrama).subscribe(value=>{
       console.log(value.data.diagram_id);
+      this.creating=false;
+      this.spinner.hide();
       //this.router.navigateByUrl(`/editor/${value.data.diagram_id}`);
     });
 
